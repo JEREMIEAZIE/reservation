8b05c811bab517e501ffdf3c9a80436938284d0b
JEREMIEAZIE <70562882+JEREMIEAZIE@users.noreply.github.com>
2025-05-24 13:00:11 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyBycr1HdRbmZtUCmr9hICvcXecvq6moMmo",
   authDomain: "reserv-5c953.firebaseapp.com",
   projectId: "reserv-5c953",
-  storageBucket: "reserv-5c953.firebasestorage.app",
+  storageBucket: "reserv-5c953.appspot.com",
   messagingSenderId: "216154940381",
   appId: "1:216154940381:web:0f61e20977edd5b517cc34",
   measurementId: "G-W0JGWJN3WM"
 };
 
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
